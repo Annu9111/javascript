ca94a111d944fd42e9fa540c8af937e627b21ae2
Annu9111 <aksoni911190@gmail.com>
2026-03-08 15:28:27 +0530

    var

---
+++ script.js
@@ -0,0 +1,14 @@
+let fullName="Annu Soni";
+let age=20;
+console.log(fullName);
+
+const a=23;
+console.log(a);
+
+const object={
+    a:"aaaa",
+    b:"bbbbb",
+    c:33333
+};
+console.log(object);
+console.log(object["a"]);
