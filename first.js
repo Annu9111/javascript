e77ceacc77c77a7bfbfb09ab4c4673d3b762c296
Annu9111 <aksoni911190@gmail.com>
2026-03-08 14:41:33 +0530

    cosole

--- first.js
+++ first.js
@@ -1,5 +1,14 @@
 console.log("Annu");
 console.log("i am learning javascript");
 
-variable="annu";
-console.log(variable);
+a="annu";
+console.log(a);
+
+b=null;
+console.log(b);
+
+c=undefined;
+console.log(c);
+
+d=true;
+console.log(d)
