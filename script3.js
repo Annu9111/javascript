e21d78712f7ad72482bd4555165fcb178e52229f
Annu9111 <aksoni911190@gmail.com>
2026-03-09 20:10:27 +0530

    operators

--- script3.js
+++ script3.js
@@ -1,1 +1,8 @@
 // console.log("hello world");  comments in js
+let a=3;
+let b=6;
+console.log("a+b = ", a + b);  
+console.log("a-b = ", a - b);  
+console.log("axb = ", a * b);  
+console.log("a/b = ", a / b);  
+console.log("a%b = ", a % b);  
