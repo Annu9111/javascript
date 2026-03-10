adb94c881e9bb006636d6d0a9ff273477497c635
Annu9111 <aksoni911190@gmail.com>
2026-03-10 19:47:26 +0530

    if

--- script3.js
+++ script3.js
@@ -7,3 +7,12 @@ console.log("axb = ", a * b);
 console.log("a/b = ", a / b);  
 console.log("a%b = ", a % b);  
 console.log("a**b = ", a ** b);  
+
+//conditional statement
+let age=2;
+if (age>18){
+    console.log("Can vote")
+}
+else{
+    console.log("cannot vote")
+}
