f0ee1a0145b26902217f74caebd8642dd2a64e25
Annu9111 <aksoni911190@gmail.com>
2026-03-10 20:00:08 +0530

    elseif

--- script3.js
+++ script3.js
@@ -11,8 +11,19 @@ console.log("a**b = ", a ** b);
 //conditional statement
 let age=2;
 if (age>18){
-    console.log("Can vote")
+    console.log("Can vote");
 }
 else{
-    console.log("cannot vote")
+    console.log("cannot vote");
+}
+
+let aa=34;
+if (aa<18){
+    console.log("junier");
+}
+else if(aa>60){
+    console.log("senior");
+}
+else{
+    console.log("middle");
 }
