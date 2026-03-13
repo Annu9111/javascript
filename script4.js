d01327d8b4c517a20f3a4b2842f98ef1843fb174
Annu9111 <aksoni911190@gmail.com>
2026-03-13 16:53:35 +0530

    while

--- script4.js
+++ script4.js
@@ -1,3 +1,15 @@
 for(let i=1;i<=5;i++){
     console.log("annu");
 }
+
+let sum=0;
+for(let i=1;i<=10;i++){
+    sum+=i;
+}
+console.log(sum);
+
+let i=0;
+while(i<5){
+    console.log(i);
+    i++;
+}
