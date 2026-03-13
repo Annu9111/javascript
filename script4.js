56cbb589fdecc4a67fc472d64fb43c7428dc1e58
Annu9111 <aksoni911190@gmail.com>
2026-03-13 17:11:12 +0530

    loops

--- script4.js
+++ script4.js
@@ -13,3 +13,27 @@ while(i<5){
     console.log(i);
     i++;
 }
+
+let str="JAVASCRIPT"
+for(let i of str){
+    console.log(i);
+
+}
+
+//object
+let object={
+    name:"Annu",
+    age:20,
+    sub:"maths"
+}
+
+for(let key in object){
+    console.log("key = ",key,"value = ",object[key]);
+}
+
+
+for(let i=0;i<100;i++){
+    if(i%2==0){
+        console.log(i);
+    }
+}
