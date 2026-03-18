24cea545bb78dc6cc3fdbd0ddf043e64bbde2a16
Annu9111 <aksoni911190@gmail.com>
2026-03-18 08:19:59 +0530

    js8

---
+++ script8.js
@@ -0,0 +1,7 @@
+let btn=document.createElement("button");
+btn.innerText="click me";
+btn.style.color="red";
+btn.style.backgroundColor="yellow";
+document.querySelector("body").prepend(btn);
+
+let para=document.querySelector("p");
