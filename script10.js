2d73a46d21832492f6c263353170d37ab6c0cc7b
Annu9111 <aksoni911190@gmail.com>
2026-03-22 16:06:50 +0530

    classes

--- script10.js
+++ script10.js
@@ -9,5 +9,18 @@ class user{
     }
 }
 
+class Admin extends user{
+    constructor(name,email){
+        super(name,email);
+    }
+
+
+    editdata(){
+        console.log("edited value");
+    }
+}
+
 let student1=new user("annu","abc@gmail.com");
 let student2=new user("anu","xyz@gamil.com");
+
+let admin1= new Admin("admin","admin@gmail.com");
